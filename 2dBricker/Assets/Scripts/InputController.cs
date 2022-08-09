46e8888830975b2833921ab4e1af57ab9b4be2db
Alexandr Lavrov <alexander_lavrov@sabgames.org>
2022-08-09 16:38:01 +0500

    Update Control

--- 2dBricker/Assets/Scripts/InputController.cs
+++ 2dBricker/Assets/Scripts/InputController.cs
@@ -1,30 +1,53 @@
+using UniRx;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class InputController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+public class InputController : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
     [Space] [SerializeField] private float _magnitudeMultiplier = 1f;
-    
+
+    public Vector3ReactiveProperty PointerWorldPosition;
+    public Vector3ReactiveProperty PointerPosition;
+
     private Vector2 _realPointerPosition;
     private Vector2 _smoothedPointerPosition;
+
+    private Camera _camera;
+    private Vector3 _cameraOffset;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+        _cameraOffset = _camera.transform.position;
+
+        PointerPosition = new Vector3ReactiveProperty(Vector3.zero);
+        PointerWorldPosition = new Vector3ReactiveProperty(Vector3.zero);
+
+        PointerPosition.Subscribe(UpdatePointerWorldPosition);
+    }
 
     private void LateUpdate()
     {
         var delta = _realPointerPosition - _smoothedPointerPosition;
         _smoothedPointerPosition += delta * Time.deltaTime * _magnitudeMultiplier;
+        PointerPosition.Value = _smoothedPointerPosition;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        //PointerPosition.Value = eventData.position;
         _realPointerPosition = eventData.position;
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         _realPointerPosition = eventData.position;
+        //PointerPosition.Value = eventData.position;
+    }
+
+    private void UpdatePointerWorldPosition(Vector3 screenPosition)
+    {
+        var worldPosition = _camera.ScreenToWorldPoint(screenPosition) - _cameraOffset;
+        PointerWorldPosition.Value = worldPosition;
     }
 }
--- 2dBricker/Assets/Scripts/PlayerHarvester.cs
+++ 2dBricker/Assets/Scripts/PlayerHarvester.cs
@@ -1,5 +1,18 @@
+using System;
 using Bricker.Game;
+using UnityEngine;
 
 public class PlayerHarvester : Harvester
 {
+    [SerializeField] private InputController _inputController;
+
+    private void Start()
+    {
+        _inputController.PointerWorldPosition.Subscribe(MoveToPosition);
+    }
+
+    private void MoveToPosition(Vector3 worldPosition)
+    {
+        transform.position = worldPosition;
+    }
 }
