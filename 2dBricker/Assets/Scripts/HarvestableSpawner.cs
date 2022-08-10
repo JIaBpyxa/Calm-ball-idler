b2ce4204ab475e749b7806fa0c91f25d926a63fe
Alexandr Lavrov <alexander_lavrov@sabgames.org>
2022-08-10 15:25:23 +0500

    Add Simple spawner

--- 2dBricker/Assets/Scripts/HarvestableSpawner.cs
+++ 2dBricker/Assets/Scripts/HarvestableSpawner.cs
@@ -1,11 +1,18 @@
+using UniRx;
 using UnityEngine;
+using Zenject;
+using TimeSpan = System.TimeSpan;
 
 namespace Bricker.Game
 {
     public class HarvestableSpawner : MonoBehaviour
     {
+        [SerializeField] private float _spawnInterval = 3f;
+        [SerializeField] private GameObject _harvestablePrefab;
+
         private IHarvestable.Factory _factory;
 
+        [Inject]
         private void Construct(IHarvestable.Factory factory)
         {
             _factory = factory;
@@ -13,8 +20,14 @@ namespace Bricker.Game
 
         private void Start()
         {
-            
-            //Observable.IntervalFrame(30).TakeUntilDisable(this).Subscribe(x => Debug.Log(x), () => Debug.Log("completed!"));
+            var aloe = Observable.Interval(TimeSpan.FromSeconds(_spawnInterval)).TakeUntilDisable(this);
+            aloe.Subscribe(l => SpawnHarvestable());
+        }
+
+        private void SpawnHarvestable()
+        {
+            var newHarvestable = (AbstractBall)_factory.Create(_harvestablePrefab);
+            newHarvestable.transform.position = transform.position;
         }
     }
 }
