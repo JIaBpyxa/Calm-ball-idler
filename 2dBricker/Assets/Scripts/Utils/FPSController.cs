23f1b08eb617a26e18ce7fe2933656d7e7de7826
Alexandr Lavrov <alexander_lavrov@sabgames.org>
2022-08-10 17:44:43 +0500

    Add FPS Control

---
+++ 2dBricker/Assets/Scripts/Utils/FPSController.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+namespace Bricker.Utils
+{
+    public class FPSController : MonoBehaviour
+    {
+        [SerializeField] private Canvas _fpsCanvas;
+        [SerializeField] private TextMeshProUGUI _fpsText;
+        [SerializeField] private bool _isShow;
+
+        private float _deltaTime;
+
+        private void Start()
+        {
+            Application.targetFrameRate = Screen.currentResolution.refreshRate / 1;
+            _fpsCanvas.gameObject.SetActive(_isShow);
+        }
+
+        private void Update()
+        {
+            if (!_isShow) return;
+
+            if (Time.timeScale != 0)
+            {
+                ShowFPS();
+            }
+        }
+
+        private void ShowFPS()
+        {
+            _deltaTime += (Time.deltaTime - _deltaTime) * 0.1f;
+            var fps = 1.0f / _deltaTime;
+            _fpsText.text = $"{Mathf.Ceil(fps)}";
+        }
+    }
+}
