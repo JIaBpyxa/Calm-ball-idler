6312baa7fee512ed2a46bffed7564da0414045ad
Alexander Lavrov <Jiabp.lad@yandex.com>
2022-09-02 13:39:00 +0500

    Update DayTime service

---
+++ 2dBricker/Assets/Scripts/Services/DayTimeColorReactor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using DG.Tweening;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Vorval.CalmBall.Service
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class DayTimeColorReactor : MonoBehaviour
+    {
+        [SerializeField] private Color _dayColor;
+        [SerializeField] private Color _nightColor;
+        [SerializeField] private float _colorChangeDuration = 1f;
+
+        private SpriteRenderer _spriteRenderer;
+
+        private DayTimeService _dayTimeService;
+
+        [Inject]
+        private void Construct(DayTimeService dayTimeService)
+        {
+            _dayTimeService = dayTimeService;
+        }
+        
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Start()
+        {
+            _dayTimeService.CurrentDayTime.Subscribe(ChangeColorByDayTime);
+        }
+
+        private void ChangeColorByDayTime(DayTimeService.DayTime dayTime)
+        {
+            var color = dayTime == DayTimeService.DayTime.Day ? _dayColor : _nightColor;
+            _spriteRenderer.DOColor(color, _colorChangeDuration).SetEase(Ease.InOutCirc);
+        }
+    }
+}
